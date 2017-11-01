a3e29cb35ec1e0c20ab74e811275cd72456af71e
AlexLeshyk <gashy_jet@yahoo.com>
2017-11-02 00:00:34 +0300

    add owl carousels

--- app/js/main-new.js
+++ app/js/main-new.js
@@ -5,6 +5,35 @@ $(document).ready(function() {
     speed: 600,
     startItem: 0
   });
+
+
+  $(".js-owl-carousel").owlCarousel({
+  	loop:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            margin:0
+        },
+        481:{
+            items:2
+        },
+        600:{
+            items:3,
+            margin:15
+        },
+        1000:{
+            items:4,
+            margin:20
+        }
+    }
+  });
+
+  $(".js-carousel").owlCarousel({
+  	items:1,
+  	dotsClass:'navigation'
+  });
+
 });
 
 $(window).load(function() {
